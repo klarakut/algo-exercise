53a24e221e84cdb16d27a263d2abc86619a988ea
KlaraKutlakova <klarakutlakova@gmail.com>
2022-11-27 14:05:15 +0100

    update examAlgo file

--- algo-exam/src/algoFinal/examAlgo.java
+++ algo-exam/src/algoFinal/examAlgo.java
@@ -10,23 +10,23 @@ public class examAlgo {
         /*
         32. Isogram
 
-Create a function called isogram that takes a string as an input and returns a boolean value whether a string is a isogram or not. Rules are as follows:
+            Create a function called isogram that takes a string as an input and returns a boolean value whether a string is a isogram or not. Rules are as follows:
 
-the comparison is case-insensitive: "a" DOES EQUAL "A"
-the space is not considered as a character (it is ignored)
-string has to be at least one character long, otherwise the result is by DEFAULT "false"
-Isogram is a word or a phrase in which no letter of the alphabet occurs more than once ("Evan" is isogram, "carrot" is NOT isogram)
+            the comparison is case-insensitive: "a" DOES EQUAL "A"
+            the space is not considered as a character (it is ignored)
+            string has to be at least one character long, otherwise the result is by DEFAULT "false"
+            Isogram is a word or a phrase in which no letter of the alphabet occurs more than once ("Evan" is isogram, "carrot" is NOT isogram)
 
-Example cases:
+            Example cases:
 
-isogram("Readily");
-Should return "true"
+            isogram("Readily");
+            Should return "true"
 
-isogram("UNCOPYRIGHTABLE");
-Should return "true"
+            isogram("UNCOPYRIGHTABLE");
+            Should return "true"
 
-isogram("He is moral");
-Should return "true"
+            isogram("He is moral");
+            Should return "true"
 
          */
         String s = "";
